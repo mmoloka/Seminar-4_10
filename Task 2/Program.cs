ff9897bd89360c1eb107abb3269a59bc8ab1944c
«Mikhail» <kopfgolova@gmail.com>
2022-06-09 20:51:26 +0300

    add Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,4 +4,17 @@
 n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са, 
 ав, ва, ви, ив, св, вс
 */
-Console.WriteLine("Hello, World!");
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if(length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for(int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[2]);
