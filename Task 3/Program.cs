24cc468306867688ed78e35da419bfa3a85c78a7
«Mikhail» <kopfgolova@gmail.com>
2022-06-09 21:30:52 +0300

    add Task 3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -8,6 +8,35 @@ data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
 info = {2, 3, 3, 1 }
 выходные данные:
 1, 7, 0, 1
-
 */
-Console.WriteLine("Hello, World!");
+
+int Translate(int[] info)
+{
+    int result = 0;
+    for (var i = 0; i < info.Length; i++)
+    {
+        result += info[info.Length - i - 1] * ((int)Math.Pow(2, i)); // 1 1 1   1*1 (1)+ 1*2 + 1* 4    0 1 2 3 
+    }
+    return result;
+}
+
+void GetNumber(int[] info, int[] data)
+{
+    int count = 0;
+    foreach (var item in data)
+    {
+        int[] temp = new int[item];
+        int j = 0;
+        for (var i = count; i < item + count; i++)
+        {
+            temp[j++] = info[i];
+        }
+        Console.Write($"{Translate(temp)}  ");
+        count += item;
+    }
+}
+
+
+int[] info = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] data = { 2, 3, 3, 1 };
+GetNumber(info, data);
